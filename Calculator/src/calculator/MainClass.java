f4c15f91eaeb00271762533a31a002873dde29c2
BorisYoYo <55288765+BorisYoYo@users.noreply.github.com>
2019-09-13 20:14:33 +0300

    Update MainClass.java

--- Calculator/src/calculator/MainClass.java
+++ Calculator/src/calculator/MainClass.java
@@ -4,7 +4,7 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Enter numbers from 1 to 10 ");
+		System.out.println("Введите числа от 1 до 10 и операцию (+,-,*,/) разделяя их пробелом";
 		System.out.println();
 		try {
 		Input.input(); // input numbers
